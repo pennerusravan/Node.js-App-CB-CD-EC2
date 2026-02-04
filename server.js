6cd9524b45fd59df66e75cdfefd2c2398065d058
pennerusravan <sravankumarpenneru344@gmail.com>
2026-02-04 09:52:04 +0530

    Disable caching and modify root response
    Updated server response and added caching headers.

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require('express');
-const path = require('path');
-
 const app = express();
 
-// Serve static files
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// Disable caching
+app.use((req, res, next) => {
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, private');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
+  next();
 });
 
-app.listen(3000, () => {
+app.get('/', (req, res) => {
+  res.send('Order App Running without CodeBuild & Lambda');
+});
+
+app.listen(3000, '0.0.0.0', () => {
   console.log('App running on port 3000');
 });
