be7b82a1685acdd687ad855b1cbab2df5e0579f3
pennerusravan <sravankumarpenneru344@gmail.com>
2026-02-04 14:27:41 +0530

    Update server.js

--- app/server.js
+++ app/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Order App Running without CodeBuild & Lambda');
+  res.send('Order App Running without CodeBuild');
 });
 
 app.listen(3000, () => console.log('App running on port 3000'));
